8966210b87ff332aa4cbbe9e01d8d1f841dcb9ce
Chris Ackerman <chris@topher.land>
2023-03-16 23:36:18 -0700

    fix wildcard versions on prepublish

--- packages/_prepublish/index.js
+++ packages/_prepublish/index.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import cp from 'node:child_process';
 import fs from 'node:fs';
 
 import { globSync } from 'glob';
@@ -17,9 +18,25 @@ if (files.length === 0) {
   process.exit(1);
 }
 
-// Object.values(packageJson.dependencies || {}).forEach((version) => {
-//   if (version === '*') {
-//     console.error('Dependency has wildcard (*) version.');
-//     process.exit(1);
-//   }
-// });
+const dependencies = Object.values(
+  JSON.parse(
+    cp.spawnSync('npm', ['list', '--json', '--omit=dev'], {
+      encoding: 'utf-8',
+      stdio: ['ignore', 'pipe', 'ignore'],
+    }).stdout,
+  ).dependencies,
+)[0].dependencies;
+
+const isPackageJsonModified = ['dependencies', 'peerDependencies', 'optionalDependencies']
+  .filter((scope) => scope in packageJson)
+  .reduce(
+    (result, scope) =>
+      Object.entries(packageJson[scope])
+        .filter(([id, version]) => version === '*' && dependencies[id].resolved.startsWith('file:'))
+        .map(([id]) => void (packageJson[scope][id] = `^${dependencies[id].version}`)).length > 0 || result,
+    false,
+  );
+
+if (isPackageJsonModified) {
+  fs.writeFileSync('./package.json', JSON.stringify(packageJson, null, 2) + '\n');
+}
