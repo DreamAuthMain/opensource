2b1bc2e6d58227060e20b508887c60b07cb9bc81
Chris Ackerman <chris@topher.land>
2023-07-19 10:56:04 -0700

    chore: Update dependencies.

--- eslint.config.js
+++ eslint.config.js
@@ -1,13 +1,3 @@
 import rational from 'eslint-config-rational';
 
-export default rational({
-  override: [
-    {
-      files: ['packages/*/examples.ts'],
-      rules: {
-        '@typescript-eslint/no-unused-vars': 'off',
-        'import/no-extraneous-dependencies': 'off',
-      },
-    },
-  ],
-});
+export default rational();
