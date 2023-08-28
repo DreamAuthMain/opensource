8a5c01c98c6656dc685f9fcef272c2ce39291138
Chris Ackerman <chris@topher.land>
2023-08-28 16:56:17 -0700

    chore (eslint-plugin): Add recommended configuration.

--- eslint.config.js
+++ eslint.config.js
@@ -2,20 +2,5 @@ import dreamauth from '@dreamauth/eslint-plugin';
 import rational from 'eslint-config-rational';
 
 export default rational({
-  override: [
-    {
-      files: ['**/*'],
-      plugins: { dreamauth },
-      rules: {
-        'dreamauth/no-throw-new': ['warn'],
-      },
-    },
-    {
-      files: ['packages/errors/**/*', '**/*.test.*'],
-      plugins: { dreamauth },
-      rules: {
-        'dreamauth/no-throw-new': ['off'],
-      },
-    },
-  ],
+  override: [...dreamauth.configs.recommended],
 });
--- packages/eslint-plugin/src/exports.ts
+++ packages/eslint-plugin/src/exports.ts
@@ -1,9 +1,33 @@
 import { type TSESLint } from '@typescript-eslint/utils';
 
+import { type messages } from './messages.js';
 import noThrowNew from './rules/no-throw-new.js';
 
-export default {
+const dreamauth = {
   rules: {
     'no-throw-new': noThrowNew,
-  } as const satisfies Record<string, TSESLint.RuleModule<string, unknown[]>>,
+  } as const satisfies Record<string, TSESLint.RuleModule<keyof typeof messages>>,
 };
+
+const plugin = Object.assign(dreamauth, {
+  configs: {
+    recommended: [
+      {
+        files: ['**/*'],
+        plugins: { dreamauth },
+        rules: {
+          'dreamauth/no-throw-new': ['warn'],
+        },
+      },
+      {
+        files: ['packages/errors/**/*', '**/*.test.*'],
+        plugins: { dreamauth },
+        rules: {
+          'dreamauth/no-throw-new': ['off'],
+        },
+      },
+    ],
+  },
+} as const);
+
+export default plugin;
