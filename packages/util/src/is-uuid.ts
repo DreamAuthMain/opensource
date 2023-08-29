f528c563f8ae96d163eeff032c11dea589f4f835
Chris Ackerman <chris@topher.land>
2023-08-29 16:01:09 -0700

    chore (util): Rename Uuid type to UuidLike.

--- packages/util/src/is-uuid.ts
+++ packages/util/src/is-uuid.ts
@@ -1,5 +1,5 @@
-export type Uuid = `${string}-${string}-${string}-${string}-${string}`;
+export type UuidLike = `${string}-${string}-${string}-${string}-${string}`;
 
-export const isUUID = (value: unknown): value is Uuid => {
+export const isUUID = (value: unknown): value is UuidLike => {
   return typeof value === 'string' && /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(value);
 };
