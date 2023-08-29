6880a92878053fb4cc3e6c9a1d83281c6a4ae270
Chris Ackerman <chris@topher.land>
2023-08-29 16:00:47 -0700

    chore (util): Rename isEmail to isEmailLike.

--- packages/util/src/is-email-like.ts
+++ packages/util/src/is-email-like.ts
@@ -1,10 +1,10 @@
-export type Email = `${string}@${string}.${string}`;
+export type EmailLike = `${string}@${string}.${string}`;
 
 /**
  * Matching doesn't guarantee a valid email address. This is just a quick
  * check that it has a name-like part, an `@` symbol, and a domain-like
  * part.
  */
-export const isEmail = (value: unknown): value is Email => {
+export const isEmailLike = (value: unknown): value is EmailLike => {
   return typeof value === 'string' && /^[^@]+@(?:[^@.]+\.)+[^@.]+$/iu.test(value);
 };
