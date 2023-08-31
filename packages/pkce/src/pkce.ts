adb757ddc146b036e1547999fe3232d4e116fc14
Chris Ackerman <chris@topher.land>
2023-08-31 13:08:07 -0700

    chore: Update deps.

--- packages/pkce/src/pkce.ts
+++ packages/pkce/src/pkce.ts
@@ -11,15 +11,19 @@ export class Pkce {
   readonly createVerifier = async (length = 128): Promise<string> => {
     const crypto = await this.#crypto();
     const bytes = crypto.getRandomValues(new Uint8Array(length));
+
     return base64UrlEncode(bytes);
   };
 
   readonly createChallenge = async (verifier: string): Promise<string> => {
     const crypto = await this.#crypto();
-    // The challenge intentionally uses the bytes of the verifier string,
-    // WITHOUT Base64URL decoding it.
+    /*
+     * The challenge intentionally uses the bytes of the verifier string,
+     * WITHOUT Base64URL decoding it.
+     */
     const bytes = new TextEncoder().encode(verifier);
     const hash = await crypto.subtle.digest('SHA-256', bytes);
+
     return base64UrlEncode(hash);
   };
 }
