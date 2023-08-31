a50314589dd1cff7b5b3a78283897ce2206bc4a6
Chris Ackerman <chris@topher.land>
2023-08-30 21:06:38 -0700

    chore (util/pick): Refactor Picked type.

--- packages/util/src/pick.ts
+++ packages/util/src/pick.ts
@@ -1,24 +1,24 @@
 export type Picked<
   TValue extends Record<string | symbol | number, any>,
-  TKeys extends readonly (string | number | symbol)[],
+  TKey extends string | number | symbol,
 > = any extends any
   ? {
-      [P in TKeys[number]]: P extends keyof TValue ? TValue[P] : unknown;
+      [P in TKey]: P extends keyof TValue ? TValue[P] : unknown;
     }
   : never;
 
 export const pick = <
   TValue extends Readonly<Record<string | symbol | number, any>>,
-  TKeys extends readonly (string | symbol | number)[],
+  TKey extends string | symbol | number,
 >(
   value: TValue,
-  ...keys: TKeys
-): Picked<TValue, TKeys> => {
+  ...keys: TKey[]
+): Picked<TValue, TKey> => {
   const result: Record<string | number | symbol, any> = {};
 
   for (const key of keys) {
     result[key] = key in value ? value[key] : undefined;
   }
 
-  return result as Picked<TValue, TKeys>;
+  return result as Picked<TValue, TKey>;
 };
