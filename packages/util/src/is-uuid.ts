17b2dba87526c92664c6c0ceec6092ba23e5dbcd
Chris Ackerman <chris@topher.land>
2023-09-13 16:01:27 -0700

    fix (util): Make isUUID regex case insensitive and unicode safe.

--- packages/util/src/is-uuid.ts
+++ packages/util/src/is-uuid.ts
@@ -1,5 +1,5 @@
 export type UuidLike = `${string}-${string}-${string}-${string}-${string}`;
 
 export const isUUID = (value: unknown): value is UuidLike => {
-  return typeof value === 'string' && /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/.test(value);
+  return typeof value === 'string' && /^[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/iu.test(value);
 };
