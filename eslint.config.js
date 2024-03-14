6d2d0850d190b0473500965f204e735f4ad243e2
Chris Ackerman <chris@topher.land>
2024-03-14 13:34:07 -0700

    chore: update eslint-config-rational and remove prettier

--- eslint.config.js
+++ eslint.config.js
@@ -2,6 +2,5 @@ import dreamauth from '@dreamauth/eslint-plugin';
 import rational from 'eslint-config-rational';
 
 export default rational({
-  ignores: ['**/{.git,node_modules,out,lib,dist}'],
   override: [...dreamauth.configs.recommended],
 });
